84bd5a05530483287b1440fbe03ee5dc8476bca4
Unknown <mattirwin089@gmail.com>
2017-12-03 15:02:00 -0500

    12/3/2017 2:34 AM 	1.) Set up SQL server 	2.) Added logo to page

--- Server/WritersCorner.php
+++ Server/WritersCorner.php
@@ -1,13 +1,38 @@
-<?php 
+<?php
 
-function TestBlockHTML ($replStr) {
-echo "<html>
-    <body><h1>{$replStr}</h1>
-    </body>
-    </html>";
+function USAGE() {
+    echo "USAGE: \n";
+    echo "GET {html}\n";
 }
 
-$ExampleInput="Test me out";
-TestBlockHTML ($ExampleInput);
+function LoadHTML ($HTMLToLoad) {
+        readfile($HTMLToLoad);
+}
+
+/*
+$host='rds-mysql-dormsocial.ct1feoczg7ml.us-east-2.rds.amazonaws.com:3306';
+$user='rajat95';
+$password='307database';
+$sql_name='DormSocialDatabase';
+$firstTimeThrough = 0;
+$connection = mysqli_connect($host,$user,$password, $sql_name);
+
+if($connection->connect_error){
+    die('404\n');
+}
+*/
+
+//Query Strings
+if (isset($_GET['html'])) {
+        $html = $_GET['html'];
+}
+
+//Homepage
+if (empty($html)) {
+        LoadHTML('HomePage/homepage.html');
+}
+else {
+        LoadHTML ($html);
+}
 
 ?>
